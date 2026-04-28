8d235d39eaf12c05254bd35156efec66d4091aa2
Avin Motie <amotie@student.ubc.ca>
2026-04-28 13:16:15 -0700

    feat: draw two circles on frame + height & width of frame

--- main.py
+++ main.py
@@ -16,9 +16,6 @@ def main():
         
         frame = draw_circles(frame, width, height)
 
-        img = cv2.circle(frame, (int(width/5), int(height/2)), int(height/5), (211, 211, 211), -1)
-        img = cv2.circle(frame, (int(4*width/5), int(height/2)), int(height/5), (211, 211, 211), -1)
-        
         cv2.imshow('AIRCHORDS', frame)
         
         if cv2.waitKey(1) == ord('q'):
