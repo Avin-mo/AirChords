920122a8b0397e4b01bd1288517a0f5094bcb175
Avin Motie <amotie@student.ubc.ca>
2026-07-11 11:07:23 +0300

    feat: commented out draw_circles in ui.py (not used in teh current project)

--- src/ui.py
+++ src/ui.py
@@ -1,6 +1,6 @@
 import cv2
 
-def draw_circles(frame, width, height):
-    img = cv2.circle(frame, (int(width/5), int(height/2)), int(height/5), (211, 211, 211), -1)
-    img = cv2.circle(frame, (int(4*width/5), int(height/2)), int(height/5), (211, 211, 211), -1)
-    return frame
+# def draw_circles(frame, width, height):
+#     img = cv2.circle(frame, (int(width/5), int(height/2)), int(height/5), (211, 211, 211), -1)
+#     img = cv2.circle(frame, (int(4*width/5), int(height/2)), int(height/5), (211, 211, 211), -1)
+#     return frame
