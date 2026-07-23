a268175d5a955d67c72724a3a425edf64dc4bba5
Avin Motie <amotie@student.ubc.ca>
2026-07-23 12:20:07 +0300

    feat: detect finger states using landmark y-axis comparison fix: swap handedness label to account for flipped image

--- handtracker.py
+++ handtracker.py
@@ -67,17 +67,36 @@ with mp_hands.Hands(
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
 
+    
+
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
+        for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+
+            label = handedness.classification[0].label
+            label = "Left" if label == "Right" else "Right"
+
+            index_up = hand_landmarks.landmark[8].y < hand_landmarks.landmark[6].y
+            middle_up = hand_landmarks.landmark[12].y < hand_landmarks.landmark[10].y
+            ring_up = hand_landmarks.landmark[16].y < hand_landmarks.landmark[14].y
+            pinky_up = hand_landmarks.landmark[20].y < hand_landmarks.landmark[18].y
+
+            if label == "Left":
+                thumb_up = hand_landmarks.landmark[4].x < hand_landmarks.landmark[3].x
+            else:
+                thumb_up = hand_landmarks.landmark[4].x > hand_landmarks.landmark[3].x
+
+            print(label, thumb_up, index_up, middle_up, ring_up, pinky_up)
+            
+        
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
     if cv2.waitKey(1) == ord('q'):
