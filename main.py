e61cb671311f58e5fe05b013c6a4e0779029adb0
Avin Motie <amotie@student.ubc.ca>
2026-07-24 09:54:11 +0300

    feat: implemented detect_chord and detect_sign to main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 import mediapipe as mp
-from src.handtracker import get_finger_states, detect_sign
+from src.handtracker import detect_chord, detect_sign, get_finger_states
 from src.ui import draw_hand_ui
 
 mp_drawing = mp.solutions.drawing_utils
@@ -11,17 +11,18 @@ def main():
     cap = cv2.VideoCapture(0)
     width = int(cap.get(3))
     height = int(cap.get(4))
-    
+
     with mp_hands.Hands(
         model_complexity=0,
         max_num_hands=2,
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as hands:
-        
+
         while cap.isOpened():
             success, image = cap.read()
             if not success:
                 continue
+            signs = {}
 
             image.flags.writeable = False
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -29,8 +30,7 @@ def main():
 
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            
-            
+
             if results.multi_hand_landmarks:
                 for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
                     mp_drawing.draw_landmarks(
@@ -39,23 +39,29 @@ def main():
                         mp_hands.HAND_CONNECTIONS,
                         mp_drawing_styles.get_default_hand_landmarks_style(),
                         mp_drawing_styles.get_default_hand_connections_style())
-                    
-                    
+
                     label = handedness.classification[0].label
                     label = "Left" if label == "Right" else "Right"
 
                     thumb, index, middle, ring, pinky = get_finger_states(hand_landmarks, label)
-                    sign = detect_sign(thumb, index, middle, ring, pinky)
-                    print(label, sign)
-                    
-                    draw_hand_ui(image, hand_landmarks, sign, width, height)
-                    
-            
+
+                    if label == "Left":
+                        result = detect_chord(thumb, index, middle, ring, pinky)
+                    else:
+                        result = detect_sign(thumb, index, middle, ring, pinky)
+
+                    signs[label] = (hand_landmarks, result)
+
             image = cv2.flip(image, 1)
 
-            if results.multi_hand_landmarks:
-                for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
-                    draw_hand_ui(image, hand_landmarks, sign, width, height)
+            for label, (hand_landmarks, result) in signs.items():
+                draw_hand_ui(image, hand_landmarks, result, width, height)
+
+            chord = signs.get("Left", (None, None))[1]
+            chord_type = signs.get("Right", (None, None))[1]
+
+            if chord and chord_type and chord != "fist" and chord_type != "fist":
+                print(f"{chord} {chord_type}")
 
             cv2.imshow('AirChords', image)
             if cv2.waitKey(1) == ord('q'):
