12b6aac2a46440b76de135a8a8919d293423828f
Avin Motie <amotie@student.ubc.ca>
2026-07-24 08:14:24 +0300

    refactor: move webcam loop to main.py and wrap in main function

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
 import cv2
-# from src.ui import draw_circles
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
 
 def main():
     cap = cv2.VideoCapture(0)
-    width = int(cap.get(3))
-    height = int(cap.get(4))
-
-
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
+    
+    with mp_hands.Hands(
+        model_complexity=0,
+        max_num_hands=2,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
         
-        frame = cv2.flip(frame, 1)
+        while cap.isOpened():
+            success, image = cap.read()
+            if not success:
+                continue
 
-        cv2.imshow('AIRCHORDS', frame)
-        
-        if cv2.waitKey(1) == ord('q'):
-            break
-        
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = hands.process(image)
+
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            
+            if results.multi_hand_landmarks:
+                for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
+                    mp_drawing.draw_landmarks(
+                        image,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style())
+                    
+                    
+
+            cv2.imshow('AirChords', cv2.flip(image, 1))
+            if cv2.waitKey(1) == ord('q'):
+                break
+
     cap.release()
     cv2.destroyAllWindows()
 
